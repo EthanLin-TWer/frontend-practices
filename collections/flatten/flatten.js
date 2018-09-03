178d503ca15ade5f6ad1c8f8df2ad475f77916ce
Linesh <linesh.simpcity@gmail.com>
2018-09-03 10:36:46 +0800

    [Linesh] feature: implement flat and sort two dimensional arary

--- collections/flatten/flatten.js
+++ collections/flatten/flatten.js
@@ -1,7 +1,9 @@
+import { chooseNoRepeatNumber } from '../filter/filter'
+
 export const twoDimensionalArrayToOne = (collection) => {
   return collection.reduce((result, element) => result.concat(element), [])
 }
 
 export const twoDimensionalArrayToOneSorted = (collection) => {
-  return [collection]
+  return chooseNoRepeatNumber(twoDimensionalArrayToOne(collection))
 }
