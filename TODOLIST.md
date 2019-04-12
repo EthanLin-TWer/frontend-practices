f5fc17e7f4964d035eef984d27d93661a2270628
Linesh <linesh.simpcity@gmail.com>
2019-04-12 20:07:08 +0800

    ♻️refactor: extract factory function

--- TODOLIST.md
+++ TODOLIST.md
@@ -2,10 +2,10 @@
 
 - [x] tasking: 19:30:27 19:32:28
 - [x] should be able to parse a flag with value defaults to true 19:32:32 19:49:37
-- should be able to parse a flag with value also parsed
+- [x] should be able to parse a flag with value also parsed
   - [x] value can be a number 19:54:28 20:02:23
   - [x] value can be a string 20:04:05
-  - value can be a boolean
+  - [x] value can be a boolean 20:06:56
 - should be able to parse multiple flags, each with values
 - should be able to parse multiple flags - one with value and one without
 - should be able to parse multiple flags - both without values
--- tdd/args/index.js
+++ tdd/args/index.js
@@ -1,6 +1,20 @@
+import { Command } from './commands/base-command'
 import { DirectoryCommand } from './commands/directory-command'
 import { LogCommand } from './commands/log-command'
 import { PortCommand } from './commands/port-command'
+
+function createCommand(alias, flag) {
+  switch (alias) {
+    case 'l':
+      return new LogCommand(flag)
+    case 'd':
+      return new DirectoryCommand(flag)
+    case 'p':
+      return new PortCommand(flag)
+    default:
+      return new Command(flag)
+  }
+}
 
 export class CommandLine {
   constructor(commands) {
@@ -11,16 +25,6 @@ export class CommandLine {
     const [, flag] = this.commands.split('-')
     const [alias] = flag.split(' ')
 
-    if (alias === 'l') {
-      return new LogCommand(flag).parse()
-    }
-    if (alias === 'p') {
-      return new PortCommand(flag).parse()
-    }
-    if (alias === 'd') {
-      return new DirectoryCommand(flag).parse()
-    }
-
-    return {}
+    return createCommand(alias, flag).parse()
   }
 }
