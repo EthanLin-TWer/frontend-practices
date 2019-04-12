59ad9b4b4f7a80bea457afd9390895daa03be61b
Linesh <linesh.simpcity@gmail.com>
2019-04-12 19:49:29 +0800

    ♻️refactor: extract get flag default value function

--- tdd/args/index.js
+++ tdd/args/index.js
@@ -3,10 +3,6 @@ const flags = [
   { alias: 'p', name: 'port', defaultValue: 8080 },
 ]
 
-const defaultValues = {
-  l: true,
-  p: 8080,
-}
 export class CommandLine {
   constructor(commands) {
     this.commands = commands
@@ -16,10 +12,14 @@ export class CommandLine {
     return flags.find((flag) => flag.alias === alias).name
   }
 
+  getDefaultValue(alias) {
+    return flags.find((flag) => flag.alias === alias).defaultValue
+  }
+
   parse() {
     const [, flag] = this.commands.split('-')
     return {
-      [this.getFlagName(flag)]: defaultValues[flag],
+      [this.getFlagName(flag)]: this.getDefaultValue(flag),
     }
   }
 }
