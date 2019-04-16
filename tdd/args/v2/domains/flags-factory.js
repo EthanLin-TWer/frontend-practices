6f833828ebeda185afb7828702c6b8e591fc873a
Linesh <linesh.simpcity@gmail.com>
2019-04-17 01:37:35 +0800

    ♻️refactor: default value assignment

--- tdd/args/v2/domains/flags-factory.js
+++ tdd/args/v2/domains/flags-factory.js
@@ -1,5 +1,10 @@
 import { Flag } from './flag'
 
 export function createFlagsFactory(name, type, defaultValue) {
-  return new Flag(name, type, defaultValue === '8080' ? 8080 : defaultValue)
+  switch (type) {
+    case 'number':
+      return new Flag(name, type, Number(defaultValue))
+    default:
+      return new Flag(name, type, defaultValue)
+  }
 }
