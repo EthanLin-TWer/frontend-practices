da7f55413a6df83c24bf603426ee1cf97c7ab98f
Linesh <linesh.simpcity@gmail.com>
2019-04-17 01:50:28 +0800

    ✨feature: should args parser ask schema for default value

--- tdd/args/v2/index.test.js
+++ tdd/args/v2/index.test.js
@@ -1,1 +1,28 @@
-describe('ArgsParser', () => {})
+import { Schema } from './domains/schema'
+
+class ArgsParser {
+  constructor(schema) {
+    this.schema = schema
+  }
+
+  parse() {
+    return [
+      {
+        getDefaultValue() {
+          return true
+        },
+      },
+    ]
+  }
+}
+
+describe('ArgsParser', () => {
+  it('should ask schema for default value', () => {
+    const schema = new Schema('l:boolean d:string:/usr/bin p:number:8080')
+    const parser = new ArgsParser(schema)
+
+    const rags = parser.parse('-l -d -p')
+
+    expect(rags[0].getDefaultValue()).toEqual(true)
+  })
+})
