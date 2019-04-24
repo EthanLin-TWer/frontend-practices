7545d7c1c002153db6cff5e43c86b478824b6c96
Linesh <linesh.simpcity@gmail.com>
2019-04-24 20:22:15 +0800

    ✨feature: some solution against fizz buzz second part?

--- tdd/fizzbuzz/index.js
+++ tdd/fizzbuzz/index.js
@@ -1,3 +1,74 @@
-export const fizzbuzz = () => {
-  return 0
+class Rule {
+  constructor(number) {
+    this.number = number
+  }
+
+  static first(conditions) {
+    return (number) => {
+      const found = conditions.find((condition) => condition(number) !== '')
+      if (found) {
+        return found(number)
+      }
+
+      return conditions.findLast(() => true)(number)
+    }
+  }
+
+  static and(conditions) {
+    return (number) => {
+      return conditions.reduce(
+        (result, condition) => result.concat(condition(number)),
+        ''
+      )
+    }
+  }
+
+  value(condition) {
+    return condition(this.number)
+  }
+}
+
+function returnFizzWhenMatchingFirstNumber(number) {
+  if (number.toString().includes('3')) {
+    return 'Fizz'
+  }
+
+  return ''
+}
+
+function returnZeroWhenMatchingEmptyValues(number) {
+  if (number.length === 0) {
+    return '0'
+  }
+
+  return ''
+}
+
+function returns(result) {
+  return {
+    whenMatching(number) {
+      return function(target) {
+        return target % number === 0 ? result : ''
+      }
+    },
+  }
+}
+
+function returnsOriginNumber(number) {
+  return number.toString()
+}
+
+export function fizzbuzz(number) {
+  return new Rule(number).value(
+    Rule.first([
+      returnZeroWhenMatchingEmptyValues,
+      returnFizzWhenMatchingFirstNumber,
+      Rule.and([
+        returns('Fizz').whenMatching(3),
+        returns('Buzz').whenMatching(5),
+        returns('Whizz').whenMatching(7),
+      ]),
+      returnsOriginNumber,
+    ])
+  )
 }
--- tdd/fizzbuzz/index.test.js
+++ tdd/fizzbuzz/index.test.js
@@ -1,5 +1,21 @@
+// 20:21:03 20:22:16 1min 13s
+
 import { fizzbuzz } from './index'
 
-it.skip('should say 1 when saying fizzbuzz(1)', () => {
-  expect(fizzbuzz(1)).toEqual('Fizz')
+test.each([
+  [1, '1'],
+  [2, '2'],
+  [3, 'Fizz'],
+  [6, 'Fizz'],
+  [5, 'Buzz'],
+  [10, 'Buzz'],
+  [15, 'FizzBuzz'],
+  [7, 'Whizz'],
+  [14, 'Whizz'],
+  [21, 'FizzWhizz'],
+  [70, 'BuzzWhizz'],
+  [35, 'Fizz'],
+  ['', '0'],
+])('fizzbuzz(%s) should return %s', (input, expected) => {
+  expect(fizzbuzz(input)).toEqual(expected)
 })
