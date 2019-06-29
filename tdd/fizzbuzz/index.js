563d1aa9b1be571a8f626e699b2ad6cf1c1afcc5
Linesh <linesh.simpcity@gmail.com>
2019-06-30 01:05:28 +0800

    ✨feature: another try

--- tdd/fizzbuzz/index.js
+++ tdd/fizzbuzz/index.js
@@ -1,3 +1,19 @@
+function shouldSayFizz(number) {
+  return number % 3 === 0 || /3/.test(number)
+}
+function shouldSayBuzz(number) {
+  return number % 5 === 0 || /5/.test(number)
+}
+
 export function fizzbuzz(number) {
-  return number.toString()
+  if (shouldSayFizz(number) && shouldSayBuzz(number)) {
+    return 'FizzBuzz'
+  }
+  if (shouldSayFizz(number)) {
+    return 'Fizz'
+  }
+  if (shouldSayBuzz(number)) {
+    return 'Buzz'
+  }
+  return number
 }
--- tdd/fizzbuzz/index.test.js
+++ tdd/fizzbuzz/index.test.js
@@ -1,5 +1,25 @@
 import { fizzbuzz } from './index'
 
 it('should return 1 when number is not divisible by 3 or 5', () => {
-  expect(fizzbuzz(1)).toEqual('1')
+  expect(fizzbuzz(1)).toEqual(1)
+})
+
+it('should return Fizz when number is divisible by 3', () => {
+  expect(fizzbuzz(3)).toEqual('Fizz')
+})
+
+it('should return Buzz when number is divisible by 5', () => {
+  expect(fizzbuzz(5)).toEqual('Buzz')
+})
+
+it('should return FizzBuzz when number is divisible by 15', () => {
+  expect(fizzbuzz(15)).toEqual('FizzBuzz')
+})
+
+it('should return Fizz when number is not divisible by 3 but has a 3 in it', () => {
+  expect(fizzbuzz(13)).toEqual('Fizz')
+})
+
+it('should return Buzz when number is not divisible by 3 but has a 5 in it', () => {
+  expect(fizzbuzz(59)).toEqual('Buzz')
 })
